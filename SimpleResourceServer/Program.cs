9c7981c6bbc41c946ba917b37a213699ea671878
Michael Guldner <migu01@dfki.de>
2018-03-19 14:52:52 +0100

    added two resources

--- SimpleResourceServer/Program.cs
+++ SimpleResourceServer/Program.cs
@@ -8,7 +8,10 @@ namespace SimpleResourceServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Resource<OInt, int> testResource = new Resource<OInt, int>();
+            Resource<int> testResource = new Resource<int>(3, "http://localhost:12345/int/");
+            Resource<bool> test2 = new Resource<bool>(true, "http://127.0.0.1:12345/bool/");
+            Console.WriteLine("Eingabe beendet Programm...");
+            Console.ReadKey();
         }
     }
 }
