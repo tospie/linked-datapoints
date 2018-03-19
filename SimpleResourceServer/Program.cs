92dd39dd77452106e159396534cb88754c1f18f0
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-19 14:53:53 +0100

    Changed signature so example builds again

--- SimpleResourceServer/Program.cs
+++ SimpleResourceServer/Program.cs
@@ -16,7 +16,7 @@ namespace SimpleResourceServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Resource<OInt, int> testResource = new Resource<OInt, int>();
+            Resource<OInt> testResource = new Resource<OInt>();
         }
     }
 }
