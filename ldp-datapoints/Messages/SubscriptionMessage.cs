985a1d64b0de5d5b71e84193654aec3e05ff40f6
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-19 16:59:46 +0100

    Removed old subscription message interface

--- ldp-datapoints/Messages/SubscriptionMessage.cs
+++ ldp-datapoints/Messages/SubscriptionMessage.cs
@@ -8,5 +8,6 @@ namespace LDPDatapoints.Messages
 {
     class SubscriptionMessage
     {
+
     }
 }
--- ldp-datapoints/Subscriptions/ISubscription.cs
+++ ldp-datapoints/Subscriptions/ISubscription.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LDPDatapoints.Messages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace LDPDatapoints.Subscriptions
 {
     public interface ISubscription<T>
     {
-        void SendMessage(ISubscriptionMessage message);
+        void SendMessage(SubscriptionMessage message);
     }
 }
