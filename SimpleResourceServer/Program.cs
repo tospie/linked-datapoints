4f96782379d551f212b112a8f2119bc581931c56
Michael Guldner <migu01@dfki.de>
2018-03-19 14:52:42 +0100

    removed OInt stub class

--- SimpleResourceServer/Program.cs
+++ SimpleResourceServer/Program.cs
@@ -3,14 +3,6 @@ using System;
 
 namespace SimpleResourceServer
 {
-    class OInt : IObservable<int>
-    {
-        public IDisposable Subscribe(IObserver<int> observer)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
