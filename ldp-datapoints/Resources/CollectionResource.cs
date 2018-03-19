d80c0c4d9244989ac5848193d63057804cc37422
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-19 12:23:55 +0100

    Added stub for colelction sub resource

---
+++ ldp-datapoints/Resources/CollectionResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDPDatapoints.Resources
+{
+    public class CollectionResource<T> : Resource<T> where T : INotifyCollectionChanged
+    {
+        protected CollectionResource(T value, string route) : base(value, route)
+        {
+        }
+    }
+}
