6cfa29be0ce70c354c48f6cabc657c29ec05b33a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-20 11:03:35 +0100

    Added PropertyResource Class

---
+++ ldp-datapoints/Resources/PropertyResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDPDatapoints.Resources
+{
+    class PropertyResource<T> : Resource<T> where T : INotifyPropertyChanged
+    {
+        public PropertyResource(T value, string route) : base(value, route)
+        {
+        }
+
+        protected override void NotifySubscriptions(object sender, EventArgs e)
+        {
+            base.NotifySubscriptions(sender, e);
+        }
+    }
+}
