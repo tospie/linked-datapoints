50f53c42950dfd72d986cc06bae60a5f6de1c89a
Michael Guldner <migu01@dfki.de>
2018-03-20 10:16:56 +0100

    TypeResource WIP

---
+++ ldp-datapoints/TypeResource.cs
@@ -0,0 +1,19 @@
+﻿using LDPDatapoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Writing;
+
+namespace ldp_datapoints
+{
+    class TypeResource
+    {
+        Graph RDFGraph { get; }
+        CompressingTurtleWriter ttlWriter { get; }
+        HttpRequestListener httpListener { get; }
+
+    }
+}
