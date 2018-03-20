52bc872c33d42e581bd7053e5f94b9e47dacd839
Michael Guldner <migu01@dfki.de>
2018-03-20 12:57:33 +0100

    ValueResource implements SubscriptionResource

--- ldp-datapoints/Resources/ValueResource.cs
+++ ldp-datapoints/Resources/ValueResource.cs
@@ -9,7 +9,7 @@ using System.Xml.Serialization;
 
 namespace LDPDatapoints.Resources
 {
-    public class ValueResource<T> : Resource<T> where T : IXmlSerializable
+    public class ValueResource<T> : SubscriptionResource<T> where T : IXmlSerializable, new()
     {
 
         protected XmlSerializer xmlSerializer;
@@ -28,6 +28,27 @@ namespace LDPDatapoints.Resources
         {
             System.IO.StringWriter stringWriter = new System.IO.StringWriter();
             xmlSerializer = new XmlSerializer(typeof(T));
+        protected override void onGet(object sender, HttpEventArgs e)
+        {
+            HttpListenerRequest request = e.request;
+            HttpListenerResponse response = e.response;
+
+            // use JSON-LD representation only if explicitly requested
+            if (request.AcceptTypes.Contains("application/ld+json") && !(request.AcceptTypes.Contains("text/turtle")))
+            {
+                // TODO: json-ld representation
+                response.OutputStream.Write(Encoding.UTF8.GetBytes("NOT YET IMPLEMENTED"), 0, "NOT YET IMPLEMENTED".Length);
+            }
+            else
+            {
+                System.IO.StringWriter stringWriter = new System.IO.StringWriter();
+                CompressingTurtleWriter ttlWriter = new CompressingTurtleWriter();
+                ttlWriter.Save(RDFGraph, stringWriter);
+                string graph = stringWriter.ToString();
+                response.OutputStream.Write(Encoding.UTF8.GetBytes(graph), 0, graph.Length);
+            }
+            response.Close();
+        }
         }
 
         protected override void NotifySubscriptions(object sender, EventArgs e)
