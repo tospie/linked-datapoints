e6f21dddf15cb67dc944d239a33375d7b9540f2d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-20 13:34:05 +0100

    Test program

--- SimpleResourceServer/Program.cs
+++ SimpleResourceServer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using LDPDatapoints;
+using LDPDatapoints.Resources;
+using LDPDatapoints.Subscriptions;
 using System;
+using System.Collections.ObjectModel;
 
 namespace SimpleResourceServer
 {
@@ -8,8 +11,16 @@ namespace SimpleResourceServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Resource<int> testResource = new Resource<int>(3, "http://localhost:12345/int/");
+            WebsocketSubscription wsSubscription = new WebsocketSubscription("ws://stuff");
+            WebHookSubscription whSubscription = new WebHookSubscription("http://test");
+            ValueResource<int> testResource = new ValueResource<int>(3, "http://localhost:12345/int/");
+            ObservableCollection<int> c = new ObservableCollection<int>();
+            CollectionResource<ObservableCollection<int>, int> r = new CollectionResource<ObservableCollection<int>, int> (c, "http://localhost:12345/c/");
+
             Resource<bool> test2 = new Resource<bool>(true, "http://127.0.0.1:12345/bool/");
+            r.Subscribe(whSubscription);
+            r.Subscribe(wsSubscription);
+            r.Value.Add(12);
             Console.WriteLine("Eingabe beendet Programm...");
             Console.ReadKey();
         }
