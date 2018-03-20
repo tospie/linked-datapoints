f878ff398da57bac1445676682e4dda4b0fe2876
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-20 08:53:24 +0100

    Implemented Collection update message

--- ldp-datapoints/Messages/CollectionUpdateMessage.cs
+++ ldp-datapoints/Messages/CollectionUpdateMessage.cs
@@ -8,5 +8,13 @@ namespace LDPDatapoints.Messages
 {
     class CollectionUpdateMessage : SubscriptionMessage
     {
+        /// <summary>
+        /// ObjectAdded = true => IndexChanged: Index of new Object, newObject = object that was added
+        /// ObjectAdded = false, newObject = null : IndexChanged = Index of Object that was removed
+        /// ObjectAdded = false, newObject != null : Property of object at index was changed; newObject is new object state
+        /// </summary>
+        public bool ObjectAdded;
+        public int IndexChanged;
+        public object newObject;
     }
 }
