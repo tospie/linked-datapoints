9837f2b46dece0d800c11bd76f733ed2a610e679
Michael Guldner <migu01@dfki.de>
2018-03-20 12:55:11 +0100

    updated program to use IXmlSerializable Resources

--- SimpleResourceServer/Program.cs
+++ SimpleResourceServer/Program.cs
@@ -1,15 +1,80 @@
-﻿using LDPDatapoints;
+﻿using LDPDatapoints.Resources;
 using System;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace SimpleResourceServer
 {
-    class Program
+    public class Program
     {
+        public class XmlInt : IXmlSerializable
+        {
+            int value;
+            public XmlInt(int value)
+            {
+                this.value = value;
+            }
+
+            public XmlInt() { }
+
+            public XmlSchema GetSchema()
+            {
+                return new XmlSchema();
+            }
+
+            public void ReadXml(XmlReader reader)
+            {
+                value = int.Parse(reader.ReadContentAsString());
+            }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteString(value.ToString());
+            }
+
+            public override string ToString()
+            {
+                return value.ToString();
+            }
+        }
+
+        public class XmlBool : IXmlSerializable
+        {
+            bool value;
+            public XmlBool(bool value)
+            {
+                this.value = value;
+            }
+
+            public XmlBool() { }
+
+            public XmlSchema GetSchema()
+            {
+                return new XmlSchema();
+            }
+
+            public void ReadXml(XmlReader reader)
+            {
+                value = reader.ReadContentAsString() == "true";
+            }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteString(value.ToString());
+            }
+
+            public override string ToString()
+            {
+                return value.ToString();
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Resource<int> testResource = new Resource<int>(3, "http://localhost:12345/int/");
-            Resource<bool> test2 = new Resource<bool>(true, "http://127.0.0.1:12345/bool/");
+            ValueResource<XmlInt> testResource = new ValueResource<XmlInt>(new XmlInt(3), "http://127.0.0.1:3333/int/");
+            ValueResource<XmlBool> test2 = new ValueResource<XmlBool>(new XmlBool(true), "http://127.0.0.1:3333/bool/");
             Console.WriteLine("Eingabe beendet Programm...");
             Console.ReadKey();
         }
