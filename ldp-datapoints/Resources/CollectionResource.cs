2aac143e30783acaee43095438c2051bad91ac2f
Michael Guldner <migu01@dfki.de>
2018-03-20 12:56:19 +0100

    CollectionResource now implements the Subscription Resource

--- ldp-datapoints/Resources/CollectionResource.cs
+++ ldp-datapoints/Resources/CollectionResource.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VDS.RDF;
 
 namespace LDPDatapoints.Resources
 {
@@ -17,7 +18,7 @@ namespace LDPDatapoints.Resources
     /// </summary>
     /// <typeparam name="T">The collection type that this Resource should represent.</typeparam>
     /// <typeparam name="U">The generic type of the collection elements.</typeparam>
-    public class CollectionResource<T, U> : Resource<T> where T : ICollection<U>, INotifyCollectionChanged
+    public class CollectionResource<T, U> : SubscriptionResource<T> where T : ICollection<U>, INotifyCollectionChanged
     {
         private bool collectionHasPropertyElements = false;
 
@@ -63,6 +64,11 @@ namespace LDPDatapoints.Resources
         {
             ObservableCollection<T> o = new ObservableCollection<T>();
             var args = e as NotifyCollectionChangedEventArgs;
+        }
+
+        protected override void onGet(object sender, HttpEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
