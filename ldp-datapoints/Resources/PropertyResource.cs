a1c2c223503316c48eac73dd69a1b16e492adc1f
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-20 12:03:37 +0100

    Removed unused usings

--- ldp-datapoints/Resources/PropertyResource.cs
+++ ldp-datapoints/Resources/PropertyResource.cs
@@ -1,11 +1,7 @@
 ﻿using LDPDatapoints.Messages;
 using LDPDatapoints.Subscriptions;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LDPDatapoints.Resources
 {
