10588ba1392f219d0adb9f6c88e78083ae698653
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-03-20 08:58:32 +0100

    Changed test server program

--- SimpleResourceServer/Program.cs
+++ SimpleResourceServer/Program.cs
@@ -1,22 +1,18 @@
 ﻿using LDPDatapoints;
+using LDPDatapoints.Resources;
 using System;
+using System.Collections.ObjectModel;
 
 namespace SimpleResourceServer
 {
-    class OInt : IObservable<int>
-    {
-        public IDisposable Subscribe(IObserver<int> observer)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Resource<OInt> testResource = new Resource<OInt>();
+            ObservableCollection<int> testCollection = new ObservableCollection<int>();
+            CollectionResource<ObservableCollection<int>, int> r = new CollectionResource<ObservableCollection<int>, int>(testCollection, "http://localhost:3333/int/");
+            testCollection.Add(12);
         }
     }
 }
