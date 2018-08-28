a898a0c2f6a1c50847a0280b91094cfea329405e
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-28 12:23:30 +0200

    Added WSSubscriptionserver class

---
+++ ldp-datapoints/Subscriptions/WebSocket/WSSubscriptionServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+
+namespace LDPDatapoints.Subscriptions.WebSocket
+{
+    public class WSSubscriptionServer
+    {
+
+        public static WSSubscriptionServer Instance { get; set; }
+
+        private WebSocketServer server;
+
+        public WSSubscriptionServer(string host, int port)
+        {
+            try
+            {
+                server = new WebSocketServer(System.Net.IPAddress.Parse(host), port);
+                server.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to open WebSocket Server under {0}, {1} . Reason: {2} ", host, port, e.Message);
+            }
+        }
+
+        public void AddSubscriptionRoute(string path, WebsocketSubscription subscription)
+        {
+            try
+            {
+                server.AddWebSocketService(path, () => new WsBehaviour(subscription));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to add WebSocketService for path {0} : {1}", path, e.Message);
+            }
+        }
+    }
+}
