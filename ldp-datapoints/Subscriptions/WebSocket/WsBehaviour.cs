39fed4e647e671f0685fd1836dfd2c24a46264f5
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-28 12:23:11 +0200

    Added WebsocketBehavior class for Subscription websocket services

---
+++ ldp-datapoints/Subscriptions/WebSocket/WsBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace LDPDatapoints.Subscriptions.WebSocket
+{
+    public class WsBehaviour : WebSocketBehavior
+    {
+        private WebsocketSubscription subscription;
+
+        public WsBehaviour(WebsocketSubscription subscription)
+        {
+            this.subscription = subscription;
+            subscription.Behaviour = this;
+        }
+
+        internal void Broadcast(string message)
+        {
+            Sessions.Broadcast(message);
+        }
+
+        internal void Broadcast(byte[] message)
+        {
+            Sessions.Broadcast(message);
+        }
+
+        protected override Task OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine("Websocket connection closed on {0} ", subscription.Route);
+            return base.OnClose(e);
+        }
+
+        protected override Task OnOpen()
+        {
+            Console.WriteLine("Received a new Websocket connection on {0} ", subscription.Route);
+            return base.OnOpen();
+        }
+    }
+}
