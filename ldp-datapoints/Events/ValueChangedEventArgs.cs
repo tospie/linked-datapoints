690ee40e38c78e02a9756a36dda4bf90d38350cd
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2019-02-20 11:15:16 +0100

    Added ValueChanged event args for datapoints

---
+++ ldp-datapoints/Events/ValueChangedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDPDatapoints.Events
+{
+    public class ValueChangedEventArgs : EventArgs
+    {
+        public object Value { get; private set; }
+
+        public ValueChangedEventArgs(object value)
+        {
+            Value = value;
+        }
+    }
+}
